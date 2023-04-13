eff0e8a990ecd4e703f49b6d5d7faa1a4bb6c1dc
jonathan-laurent <jonathan.laurent@cs.cmu.edu>
2023-04-13 20:08:57 +0200

    Remove exposed internal

--- src/tql_output.mli
+++ src/tql_output.mli
@@ -6,8 +6,6 @@ val snapshot_counter : int ref
 
 val snapshots_native_format : bool ref
 
-val output_directory : string ref
-
 val set_output_directory : string -> unit
 
 val file : string -> string
