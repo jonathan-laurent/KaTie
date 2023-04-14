513fefd9ed2e068a6901d025afceef14d8492887
jonathan-laurent <jonathan.laurent@cs.cmu.edu>
2023-04-14 13:48:08 +0200

    Compute all matchings: done

--- src/query_eval.ml
+++ src/query_eval.ml
@@ -2,9 +2,6 @@
 (* New query evaluator                                                       *)
 (*****************************************************************************)
 
-open Aliases
-open Query
-
 (* A query is evaluated in several steps:
     1. We first cache the results of [Event_matcher.match_event] for
        each event in the trace.
@@ -15,11 +12,11 @@ open Query
 (* Utilities                                                                 *)
 (*****************************************************************************)
 
-let number_of_events q = Array.length q.pattern.events
+let number_of_events q = Array.length q.Query.pattern.events
 
-let number_of_agents q = Array.length q.pattern.agents
+let number_of_agents q = Array.length q.Query.pattern.agents
 
-let query_root_event q = List.hd q.pattern.execution_path
+let query_root_event q = List.hd q.Query.pattern.execution_path
 
 (*****************************************************************************)
 (* Link cache computation                                                    *)
@@ -27,7 +24,7 @@ let query_root_event q = List.hd q.pattern.execution_path
 
 (* A "link" is an ordered matching of all agents in [event.link_agents] *)
 module Link = struct
-  type t = global_agent_id list [@@deriving show, yojson]
+  type t = Aliases.global_agent_id list [@@deriving show, yojson]
 
   let pp = Fmt.(list int)
 
@@ -50,23 +47,23 @@ module LinkCache = struct
   let create query =
     Cache (Array.init (number_of_events query) (fun _ -> LinkMap.empty))
 
-  let add (Cache cache) ev_id step_id_in_trace pot_match =
-    let ev_cache = cache.(ev_id) in
+  let add (Cache cache) ~ev_lid ~ev_gid pot_match =
+    let ev_cache = cache.(ev_lid) in
     let link = pot_match.Event_matcher.link in
     let hist =
       LinkMap.find_opt link ev_cache
       |> Option.value ~default:History.empty
-      |> History.add step_id_in_trace pot_match.status
+      |> History.add ev_gid pot_match.status
     in
-    cache.(ev_id) <- LinkMap.add link hist ev_cache
+    cache.(ev_lid) <- LinkMap.add link hist ev_cache
 
-  let access_cache f cache ev_id link ref_step_id =
-    let ev_cache = cache.(ev_id) in
+  let access_cache f cache ev_lid link ref_ev_gid =
+    let ev_cache = cache.(ev_lid) in
     match LinkMap.find_opt link ev_cache with
     | None ->
         None
     | Some hist ->
-        f ref_step_id hist
+        f ref_ev_gid hist
 
   let first_after cache = access_cache History.first_after cache
 
@@ -74,26 +71,29 @@ module LinkCache = struct
 end
 
 let compute_link_cache_step query window cache =
-  query.pattern.events
-  |> Array.iteri (fun ev_id ev ->
+  query.Query.pattern.events
+  |> Array.iteri (fun ev_lid ev ->
          match Event_matcher.match_event ev window with
          | None ->
              ()
          | Some pot_match ->
-             LinkCache.add cache ev_id window.step_id pot_match )
+             LinkCache.add cache ~ev_lid ~ev_gid:window.step_id pot_match )
 
 (*****************************************************************************)
 (* Enumerate all matchings                                                   *)
 (*****************************************************************************)
 
 (* Arrays indexed by [local_event_id] and [local_agent_id] respectively *)
-type matching = M of {evs: global_event_id array; ags: global_agent_id array}
+type matching =
+  | M of {evs: Aliases.global_event_id array; ags: Aliases.global_agent_id array}
 [@@deriving show, yojson_of]
 
 type root_matching =
-  | RM of {ev: global_event_id; constrained: global_agent_id list}
+  | RM of
+      {ev: Aliases.global_event_id; constrained: Aliases.global_agent_id list}
 [@@deriving show, yojson_of]
 
+(* Compute the list of all matchings of the root event *)
 let compute_root_matchings query (LinkCache.Cache link_cache) =
   let root_id = query_root_event query in
   match LinkMap.bindings link_cache.(root_id) with
@@ -114,54 +114,75 @@ let compute_root_matchings query (LinkCache.Cache link_cache) =
   | _ ->
       assert false
 
+(* This exception is raised within [complete_matching] when an
+   inconsistency is detected and therefore the partial matching must be
+   thrown away *)
 exception No_match
 
-let complete_matching query link_cache (RM root) : matching option =
+type agent_matching_list =
+  | AML of (Aliases.local_agent_id * Aliases.global_agent_id) list
+
+type partial_matching_delta =
+  | PMD of {ev_lid: int; ev_gid: int; eqs: agent_matching_list}
+
+(* In a partial matching, non-matched ids are mapped to -1. *)
+let update_partial_matching (M m) (PMD {ev_lid; ev_gid; eqs= AML eqs}) =
+  m.evs.(ev_lid) <- ev_gid ;
+  eqs
+  |> List.iter (fun (lid, gid) ->
+         if m.ags.(lid) <> -1 && m.ags.(lid) <> gid then raise No_match ;
+         m.ags.(lid) <- gid )
+
+let root_delta (RM root) ~ev_lid ev =
+  let eqs =
+    AML (Utils.list_zip ev.Query.other_constrained_agents root.constrained)
+  in
+  PMD {ev_lid; ev_gid= root.ev; eqs}
+
+let nonroot_delta (LinkCache.Cache link_cache) (M m) ~ev_lid ev =
+  let ref_ev_lid, access_cache =
+    match ev.Query.defining_rel with
+    | None ->
+        assert false (* A nonroot event must have a defining relation *)
+    | Some (First_after (ref_ev_lid, _)) ->
+        (ref_ev_lid, LinkCache.first_after)
+    | Some (Last_before (ref_ev_lid, _)) ->
+        (ref_ev_lid, LinkCache.last_before)
+  in
+  let ref_ev_gid = m.evs.(ref_ev_lid) in
+  assert (ref_ev_gid <> 1) ;
+  let link = List.map (fun i -> m.ags.(i)) ev.link_agents in
+  match access_cache link_cache ev_lid link ref_ev_gid with
+  | None | Some (_, Event_matcher.Failure) ->
+      raise No_match
+  | Some (ev_gid, Success {other_constrained}) ->
+      let eqs =
+        AML
+          ( Utils.list_zip ev.link_agents link
+          @ Utils.list_zip ev.other_constrained_agents other_constrained )
+      in
+      PMD {ev_lid; ev_gid; eqs}
+
+let complete_matching query link_cache (RM root) =
   let evs = Array.make (number_of_events query) (-1) in
   let ags = Array.make (number_of_agents query) (-1) in
-  let root_id = query_root_event query in
+  let m = M {evs; ags} in
   try
     query.pattern.execution_path
-    |> List.iter (fun ev_id ->
-           let ev = query.pattern.events.(ev_id) in
-           let step_id, eqs =
-             if ev_id = root_id then
-               (* For the root, we just use the provided root matching *)
-               ( root.ev
-               , Utils.list_zip ev.other_constrained_agents root.constrained )
-             else
-               (* For a non-root node, we use the link cache *)
-               match ev.defining_rel with
-               | None ->
-                   assert
-                     false (* A nonroot event must have a defining relation *)
-               | Some (First_after (ref_ev_id, _)) -> (
-                   let ref_ev_gid = evs.(ref_ev_id) in
-                   assert (ref_ev_gid <> 1) ;
-                   let link = List.map (fun i -> ags.(i)) ev.link_agents in
-                   match
-                     LinkCache.first_after link_cache ev_id link ref_ev_gid
-                   with
-                   | None | Some (_, Event_matcher.Failure) ->
-                       raise No_match
-                   | Some (ev_gid, Success {other_constrained}) ->
-                       ( ev_gid
-                       , Utils.list_zip ev.link_agents link
-                         @ Utils.list_zip ev.other_constrained_agents
-                             other_constrained ) )
-               | Some (Last_before (_ref_ev_id, _)) ->
-                   assert false
+    |> List.iter (fun ev_lid ->
+           let ev = query.pattern.events.(ev_lid) in
+           let delta =
+             if ev_lid = query_root_event query then
+               root_delta (RM root) ~ev_lid ev
+             else nonroot_delta link_cache m ~ev_lid ev
            in
-           evs.(ev_id) <- step_id ;
-           eqs
-           |> List.iter (fun (lid, gid) ->
-                  if ags.(lid) <> -1 && ags.(lid) <> gid then raise No_match ;
-                  ags.(lid) <- gid ) ) ;
+           update_partial_matching m delta ) ;
     Some (M {evs; ags})
   with No_match -> None
 
 let compute_all_matchings query link_cache =
   compute_root_matchings query link_cache
+  |> List.filter_map (complete_matching query link_cache)
 
 (*****************************************************************************)
 (* Main function                                                             *)
