cf7b324db0efda008ce56f3b3826cf6b39b27aca
jonathan-laurent <jonathan.laurent@cs.cmu.edu>
2023-04-17 15:15:37 +0200

    Improve example execution script

--- exec.sh
+++ exec.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # An example script to execute KaTie
-# Example usage: ./exec.sh test/large/catphos
+# Usage: ./exec.sh TEST_DIR [OTHER_KATIE_ARGS]
+# Usage example: ./exec.sh tests/large/catphos/ --debug-level 2 --no-color
 # You need to have KaSim and KaTie in your PATH (run `dune install` for the latter)
 
-DIR=$1 ; cd $DIR
+DIR=$1 ; shift ; cd $DIR
 echo "Running in directory: $DIR"
 
 KaSim model.ka -seed 0 -trace trace.json -d kasim-output
 echo -e "\n\n"
-KaTie --debug-level 2 -t kasim-output/trace.json -q query.katie --output-dir katie-output
+KaTie -t kasim-output/trace.json -q query.katie --output-dir katie-output "$@"
