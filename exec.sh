0fd5bd9f26685a88728c4515d1524b982df2b488
jonathan-laurent <jonathan.laurent@cs.cmu.edu>
2023-04-17 16:43:31 +0200

    Find Kasim bug and update Hector's unit test

--- exec.sh
+++ exec.sh
@@ -8,6 +8,7 @@
 DIR=$1 ; shift ; cd $DIR
 echo "Running in directory: $DIR"
 
+rm -rf $DIR/kasim-output $DIR/katie-output
 KaSim model.ka -seed 0 -trace trace.json -d kasim-output
 echo -e "\n\n"
 KaTie -t kasim-output/trace.json -q query.katie --output-dir katie-output "$@"
---
+++ exec_debug.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script is similar to 'exec.sh' but it uses 'dune exec' to allow for
+# debugging the source code of KaSim and KaTie simultaneously.
+# To do so, just clone both repos in the same parent folder and run this script from
+# the parent folder.
+
+DIR=$1 ; shift ; cd $DIR
+echo "Running in directory: $DIR"
+
+rm -rf $DIR/kasim-output $DIR/katie-output
+dune exec KaSim -- model.ka -seed 0 -trace trace.json -d kasim-output
+echo -e "\n\n"
+dune exec KaTie -- -t kasim-output/trace.json -q query.katie --output-dir katie-output "$@"
