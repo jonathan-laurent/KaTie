4d7e568d525329d501f72c64e1d4053d0b9e57f0
jonathan-laurent <jonathan.laurent@cs.cmu.edu>
2023-04-18 08:54:27 +0200

    Fix bug in exec.sh

--- exec_debug.sh
+++ exec_debug.sh
@@ -8,7 +8,7 @@
 DIR=$1 ; shift ; cd $DIR
 echo "Running in directory: $DIR"
 
-rm -rf $DIR/kasim-output $DIR/katie-output
+rm -rf kasim-output katie-output
 dune exec KaSim -- model.ka -seed 0 -trace trace.json -d kasim-output
 echo -e "\n\n"
 dune exec KaTie -- -t kasim-output/trace.json -q query.katie --output-dir katie-output "$@"
