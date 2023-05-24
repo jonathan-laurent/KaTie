facef819a838bd66317b9eb9cbae305954d957ae
jonathan-laurent <jonathan.laurent@cs.cmu.edu>
2023-05-24 20:08:52 +0200

    Test nontrivial query on wnt trace

--- misc/wnt-paper/run.sh
+++ misc/wnt-paper/run.sh
@@ -1,4 +1,4 @@
 # dune exec ../src/main.bc -- --no-progress-bars -q testing_query.katie -t rep_0_trace_event/trace.json --output-dir out
 
-QUERY=trivial.katie
+QUERY=main.katie
 dune exec KaTie -- -q $QUERY -t traces/rep_0_trace_event/trace.json --output-dir out
