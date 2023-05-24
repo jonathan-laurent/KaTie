8004e453140dc49d5562e16ec3c4d6d4f1bd5b9d
Jonathan Laurent <jonathan.laurent@cs.cmu.edu>
2023-05-24 17:11:39 +0200

    Update profiling script

---
+++ misc/wnt-paper/profile_trivial.sh
@@ -0,0 +1,4 @@
+rm -f perf.data
+perf record -g --call-graph=dwarf -- dune exec KaTie -- -q trivial.katie -t traces/rep_0_trace_event/trace.json --output-dir out
+perf script > perf.out
+rm -f perf.data
