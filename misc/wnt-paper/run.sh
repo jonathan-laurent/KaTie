1fda2ac37e0a050c927f8b0f7306b2e1329d917f
jonathan-laurent <jonathan.laurent@cs.cmu.edu>
2023-05-25 07:54:57 +0200

    Fix wnt paper query

--- misc/wnt-paper/run.sh
+++ misc/wnt-paper/run.sh
@@ -1,4 +1,4 @@
 # dune exec ../src/main.bc -- --no-progress-bars -q testing_query.katie -t rep_0_trace_event/trace.json --output-dir out
 
-QUERY=main.katie
+QUERY="$1.katie"
 dune exec KaTie -- -q $QUERY -t traces/rep_0_trace_event/trace.json --output-dir out
